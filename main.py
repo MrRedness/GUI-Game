468dae86ed71f950b79ac491ff6402e5572cba4b
team 1771's personal <16584585+MrRedness@users.noreply.github.com>
2024-01-23 00:45:53 -0500

    playable?

--- main.py
+++ main.py
@@ -117,17 +117,67 @@ def runFlappy():
                     bird_vel_y = bird_flap_vel
                     bird_flapped = True
 
-        print("hi")
+        # print("hi")
 
-        if isGameOver(up_pipes, down_pipes):
-            return
+        # if isGameOver(up_pipes, down_pipes):
+            # return
+        
+        player_mid_pos = player_pos.x + game_imgs['player'].get_width() / 2
+
+        for pipe in up_pipes:
+            pipe_mid_pos = pipe['x'] + game_imgs["pipe"].get_width() / 2
+            if pipe_mid_pos <= player_mid_pos < pipe_mid_pos + 4 : # Player crosses pipe
+                score += 1
+                print(f"Score is now {score}")
+        
+        # "Falling" when not fllapping
+        if bird_vel_y < bird_max_vel_y and not bird_flapped :
+            bird_vel_y += bird_accl_y
+        
+        # Only flap once
+        if bird_flapped:
+            bird_flapped = False
+
+        # Update player height
+        player_pos.y += min(bird_vel_y, window_height - player_pos.y - game_imgs['player'].get_width())
+
+        # Moves pipes across the screen
+        for upper_pipe, lower_pipe in zip(up_pipes, down_pipes):
+            upper_pipe['x'] += pipe_vel_x
+            lower_pipe['x'] += pipe_vel_x
+        # When the first pipe is about to leave the screen, add another
+            
+        if 0 < up_pipes[0]['x'] < 5:
+            newpipe = createPipe() 
+            up_pipes.append(newpipe[0])
+            down_pipes.append(newpipe[1])
+
+        # Remove pipes off the screen
+        if up_pipes[0]['x'] < -game_imgs['pipe'].get_width():
+            up_pipes.pop(0)
+            down_pipes.pop(0)
+        
+        # Add images onto screen
+        window.blit(game_imgs['background'], (0,0))
+        for upper_pipe, lower_pipe in zip(up_pipes, down_pipes):
+            window.blit(game_imgs["pipe"], (upper_pipe['x'], upper_pipe['y']))
+            window.blit(game_imgs["pipe"], (lower_pipe['x'], lower_pipe['y']))
+        
+        window.blit(game_imgs['player'], (player_pos.x, player_pos.y))
+
+        # Num of digits of score
+        # numbers = [int(x) for x in list(str(score))]
+
+        pygame.display.update()
+
+        fps_clck.tick(fps)
             
   
 
 def processStartInputs():
     for event in pygame.event.get():
         if isQuitGameInput(event):
-            quitGame
+            quitGame()
         elif isRunGameInput(event):
             runFlappy()
 
